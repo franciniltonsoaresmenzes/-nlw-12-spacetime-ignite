30344b46569e309d3bf90f9453f398e1cf26c62c
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-19 23:59:36 -0300

    Aula 03

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-
-import blurBg from './src/assets/bg-blur.png'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import { useEffect } from 'react'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
+import * as WebBrowser from 'expo-web-browser'
 import { styled } from 'nativewind'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { useRouter } from 'expo-router'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 import {
   Roboto_400Regular,
@@ -12,16 +19,59 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
 const StyledStripes = styled(Stripes)
 
+WebBrowser.maybeCompleteAuthSession()
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/86e56193204add5323af',
+}
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonst] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '86e56193204add5323af',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonst) {
     return null
@@ -51,6 +101,7 @@ export default function App() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={() => signInWithGithub()}
           className="rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black">
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,10 +11,16 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => console.log('HTTP Server runnig on http://localhost:3333'))
