bcb688082cba8bbd9efd2f8d35d0c0b1d87b1b12
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-05-22 15:19:59 -0300

    Aula 05

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '10.0.0.180',
+        port: '3333',
+      },
+    ],
   },
 }
 
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,16 +1,15 @@
+import './globals.css'
+import { ReactNode } from 'react'
 import {
-  Bai_Jamjuree as BaiJamjuree,
   Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
 
-import { Copyright } from '@/components/Copyright'
 import { Hero } from '@/components/Hero'
 import { Profile } from '@/components/Profile'
 import { SignIn } from '@/components/SignIn'
-
+import { Copyright } from '@/components/Copyright'
 import { cookies } from 'next/headers'
-import { ReactNode } from 'react'
-import './globals.css'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
@@ -23,7 +22,7 @@ const baiJamjuree = BaiJamjuree({
 export const metadata = {
   title: 'NLW Spacetime',
   description:
-    'Uma cápsula do tempo construído com React, Next.js, Tailwind Css e Typescript',
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript.',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -36,11 +35,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       >
         <main className="grid min-h-screen grid-cols-2">
           {/* Left */}
-          <div className="relative flex flex-col items-center justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
             {/* Blur */}
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
             {/* Stripes */}
-            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
             {isAuthenticated ? <Profile /> : <SignIn />}
             <Hero />
@@ -48,7 +48,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16 ">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
